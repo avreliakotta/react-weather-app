5c2613c9c43f76dbbe3158c72c1bf6ddeaf25de0
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-03-02 14:55:11 +0200

    Removed  component Fo

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,49 +1,59 @@
 import React from "react";
+import "./Forecast.css";
 import ReactAnimatedWeather from 'react-animated-weather';
- 
 
-export default function Forecast(response) {
-    return (
-        
-        <ul>
-            <li>
-                <ReactAnimatedWeather
-                    icon="SNOW"
-                    color="blue"
-                    size={50}
-                    animate={true}
-                />
-            </li>
-            <li>
-                <ReactAnimatedWeather
-                    icon="CLEAR_DAY"
-                    color="blue"
-                    size={50}
-                    animate={true}
-                />
-            </li>
-            <li><ReactAnimatedWeather
-                icon="RAIN"
-                color="blue"
-                size={50}
-                animate={true}
-            /></li>
-            <li>
-                <ReactAnimatedWeather
-                    icon="CLEAR_DAY"
-                    color="blue"
-                    size={50}
-                    animate={true}
-                />
-            </li>
-            <li>
-                <ReactAnimatedWeather
-                    icon="CLEAR_DAY"
-                    color="blue"
-                    size={50}
-                    animate={true}
-                />
-            </li>
+export default function Forecast(responsive) {
+   
+     return (
+        <div className="Forecast">
+           
+           <ul className="Forecast-list">
+              <li>
+                 <p>Mon</p>
+                 <ReactAnimatedWeather
+    icon="SNOW"
+    color="blue"
+    size={40}
+    animate={true}
+           /></li>
+           
+              <li>
+                 <p>Tue</p>
+                 <ReactAnimatedWeather
+    icon="RAIN"
+    color="blue"
+    size={40}
+    animate={true}
+              /></li>
+              <li>
+                 <p>Wed</p>
+                 <ReactAnimatedWeather
+    icon="FOG"
+    color="blue"
+    size={40}
+    animate={true}
+                 /></li>
+              <li>
+                 <p>Thur</p>
+                 <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="blue"
+    size={40}
+    animate={true}
+  />
+              </li>
+              <li>
+                 <p>Fri</p>
+                 <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="blue"
+    size={40}
+    animate={true}
+  />
+              </li>
+              
         </ul>
+           
+     </div>
     );
 }
